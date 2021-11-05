933a55e6440ec1fddf1c5be30c5847705b15af6c
Polina <plnsnegurets@gmail.com>
2021-11-05 21:53:41 +0200

    Initial commit

---
+++ src/com/company/Task1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int year = 3126;
+        int yearFriend = (year - 8);
+        System.out.println(yearFriend);
+    }
+}
---
+++ src/com/company/Task2.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+//        int h = 20;
+//        int m = 60;
+//        System.out.println("Минуты: " + h*m);
+
+//        int a = 5, b = 4;
+//        int S = (a*b)/2;
+//        System.out.println("Площадь: " + S);
+
+//        int number1 = 2, number2 = 3, number3 = 5;
+//        double number4 = (Math.pow(number1, number1) + Math.pow(number2,number1) + Math.pow(number3, number1))/3;
+//        System.out.println("Cреднее арифметическое квадратов: " + number4);
+
+//        float tF = 115.5f;
+//        float tC = (tF - 32) * 5 / 9;
+//        System.out.println("Tемпература в градусах Целсии: " + tC);
+//        double tK = tC + 273.15;
+//        System.out.println("Температура в градусах Кельвины: " + tK);
+    }
+}
